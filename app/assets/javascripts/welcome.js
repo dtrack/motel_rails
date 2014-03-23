3d91cc485f58a744242ee9a9dcb7483789c0f704
Jeremy Henault <jeremy@jhenault.net>
2014-03-23 02:12:56 -0700

    fix pager bug due to slow thumbnails loading

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -6,12 +6,8 @@ $(document).ready(function() {
         useCSS: false,
         onRedraw: function(viewportDimensions) {
             var viewportHeight = viewportDimensions.height;
-            var pager = $('#bx-pager');
-            // we don't want to constrain the height
-            var oldWidth = pager.width();
-            var newWidth = oldWidth * viewportHeight / pager.height();
             pager.css({
-                width: newWidth
+                'min-height': viewportHeight
             });
         }
     });
