c1272152d22e7afada76f7e6b3727d44a781f004
Jeremy Henault <jeremy@jhenault.net>
2014-03-23 01:52:52 -0700

    add images to slideshow through controller var

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,22 @@
 class WelcomeController < ApplicationController
   def index
+    @slideshow = [
+        {
+            :name => "motel-from-road",
+            :caption => "The motel is located on Mother Road Route 66."
+        },
+        {
+            :name => "motel-king-room",
+            :caption => "Our spacious King room comes fully equiped."
+        },
+        {
+            :name => "motel-pool",
+            :caption => "Finish your day in our refreshing pool."
+        },
+        {
+            :name => "needles-wagon",
+            :caption => "Needles famous landmark, the Needlles Wagon"
+        },
+    ]
   end
 end
