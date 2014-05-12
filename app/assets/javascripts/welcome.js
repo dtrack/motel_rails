9e2c72649ffac686b8fa460ee2f9f55ca70cee6b
Jeremy Henault <jeremy@jhenault.net>
2014-05-11 18:21:29 -0700

    logo palm tree

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -4,6 +4,9 @@ $(document).ready(function() {
         pagerCustom: '#bx-pager',
         captions: true,
         useCSS: false,
+        pause: 5000,
+        auto: true,
+        autoHover: true,
         onRedraw: function(viewportDimensions) {
             var viewportHeight = viewportDimensions.height;
             $('#bx-pager').css({
